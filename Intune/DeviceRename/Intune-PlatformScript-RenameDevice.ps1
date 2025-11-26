db951627d617eac14e867d81e96f37014635f063
Alex-McDowell88 <alex.mcdowell@itechnologies.com.au>
2025-11-26 14:36:16 +1000

    Update Intune-PlatformScript-RenameDevice.ps1

--- Intune/DeviceRename/Intune-PlatformScript-RenameDevice.ps1
+++ Intune/DeviceRename/Intune-PlatformScript-RenameDevice.ps1
@@ -5,19 +5,32 @@
     .DESCRIPTION
         This script completes the following tasks
         
-        1. Queries the devices public IP to obtain the state the device is being enrolled in.
+        1. Queries the devices' public IP to obtain the state the device is being enrolled in.
 
-        2. Queries the devices serial number and truncates it to 6 characters.
+        2. Queries the devices' serial number and truncates it to 6 characters.
 
-        3. The device is named using the following convention:
+        3. The device name uses the following convention:
 
-            INT-<STATE>-<SERIAL>
+        INT-<STATE>-<SERIAL>
+
+        4. The device is rebooted using a scheduled task after Windows Hello for Business is configured.        
         
     .NOTES
         TBC.
         
 #>
 
+# Create the temp directory
+$tempdir = "c:\Temp"
+New-Item $tempdir -ItemType Directory -Force
+Set-Location -Path $tempdir
+
+# Pull files & register the scheduled task to reboot
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Alex-McDowell88/Dev/refs/heads/main/Intune/DeviceRename/Intune-RenameDevice-Reboot.ps1" -OutFile .\Intune-RenameDevice-Reboot.ps1
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Alex-McDowell88/Dev/refs/heads/main/Intune/DeviceRename/Intune-RenameDevice-Reboot.xml" -OutFile .\Intune-RenameDevice-Reboot.xml
+Register-ScheduledTask -xml (Get-Content '.\Intune-RenameDevice-Reboot.xml' | Out-String) -TaskName "Intune-RenameDevice-Reboot" -Force
+
+# Query for variables
 $ipaddress = (Invoke-WebRequest -uri "https://api.ipify.org/").Content
 $geoinfo = Invoke-RestMethod -Method Get -Uri "http://ip-api.com/json/$IPAddress"
 $serial = Get-WmiObject win32_bios | Select-Object -ExpandProperty "Serialnumber"
@@ -25,7 +38,7 @@ $suffix = $serial.SubString(0,6)
 $prefix = 'INT-'
 $hostname = ($prefix) + ($geoinfo.region) + "-" + ($suffix)
 
-
+# Rename device
 try {
     Rename-Computer -NewName $hostname
 }
